56a5fd0e922fbf0a5c67c65d128d9791f8b7e5e2
andreavieira <vieirandreas@gmail.com>
2019-10-08 14:04:59 -0400

    Updating index.js with DB Setup

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "apiKey",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com",
+    storageBucket: "bucket.appspot.com"
+  };
+  firebase.initializeApp(config);
+  
+var database = firebase.database();
+
