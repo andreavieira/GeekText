7ef860b9551e35f03e962eceee9e2384db80dba2
nrodr047 <nrodr047@fiu.edu>
2019-11-02 08:05:33 -0400

    Database collections are appended and shown in realtime, added 'save for later' button/container

--- public/shoppingapp.js
+++ public/shoppingapp.js
@@ -8,21 +8,39 @@ const cartDocRef = db.collection("users").doc("nrodr047").collection("cart")
 const outputDescript = document.querySelector("#description")
 const outputImage = document.querySelector("#item-image")
 const outputPrice = document.querySelector("#item-price")
-const cartList = document.querySelector("#cart-items")
+const dlist = document.querySelector("#cart-row")
+const cartList = document.querySelector("#cart-list")
 
 function renderCart(doc){
+    let li = document.createElement('li');                  //creates a line for the nodes
+    let description = document.createElement('div');       //creates a span for title/author
+    let price = document.createElement('div');             //creates a span for price
+    //let image = document.createElement('div');
 
-    let bkTitle = doc.get("title");
-    let bkAuthor = doc.get("authorName");
-    //const bkImage = doc.get("image");
-    let bkPrice = doc.get("price");
-    outputDescript.innerHTML = bkTitle + " By: " + bkAuthor;
-    //outputImage = bkImage;
-    outputPrice.innerHTML = "$" + bkPrice;
+    li.setAttribute('data-id', doc.id);                     //sets the data-id to the doc.id for parent node
 
+    
+    title = doc.get("title");                               //gets the title field from database
+    author = doc.get("authorName");                         //gets authorname from database
+    //image = doc.get("image");
+    
+    let bkPrice= doc.get("price");                          //tmp var for price from database
+    price.textContent = "$" + bkPrice;                      //sets price with '$'
+    description.textContent = " " + title + " By: " + author + " ";     //sets description to title and author
+
+
+    li.appendChild(description);                                //appends decription to node
+    li.appendChild(price);
+
+
+    outputDescript.innerHTML = ((cartList.appendChild(li).firstChild).textContent);
+    outputPrice.innerHTML = ((cartList.appendChild(li).lastChild).textContent);
+
+    //probably need a for loop to creat another row in my code
 }
 
 getRealtimeUpdates = function(){
+    let i = 0;
     let allItems = cartDocRef.get()
         .then(snapshot => {
             snapshot.forEach(doc =>{
@@ -69,9 +87,9 @@ function ready(){
  */
 function removeCartItem(event){
     var buttonClicked = event.target
-    let id = buttonClicked.parentElement.parentElement.getAttribute('data-id');
+    let id = buttonClicked.parentElement.parentElement.get(doc.id);
     db.collection('users').doc('nrodr047').collection('cart').doc(id).delete();
-    updateCartTotal()
+    updateCartTotal();
 }
 
 /**
